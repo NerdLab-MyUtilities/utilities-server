a25a975e31ca1911a97555250adb9c2796c0b591
mytrofanov <mytrofanovmaxym@gmail.com>
2023-07-26 09:17:23 +0300

    feat: env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000, () => {
-    console.log('Server started on port:', 3000);
+  await app.listen(PORT, () => {
+    console.log('Server started on port:', PORT);
   });
 }
 bootstrap();
